233a1b3e18c9eb1ea6e23d6b5717ff936c49a100
agung.hidayat <agung.hidayat@kalbenutritionals.com>
2021-05-22 15:42:16 +0700

    change base end point

--- app/src/main/java/com/nur_hidayat_agung/bkmmobile/util/Constant.java
+++ app/src/main/java/com/nur_hidayat_agung/bkmmobile/util/Constant.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Constant {
 
-    public static String baseUrl = "https://berkatkarimar.com/restapi/";
+    public static String baseUrl = "http://berkatkarimar.co.id/restapi/";
     public static int conTimeOut = 30;
     public static String appName = "BKMMobile";
     public static String Token = "";
